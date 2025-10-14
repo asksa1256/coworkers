0baa61a82d60370a2210234648f73c0ea3cc25aa
leesangdal <asksa1256@gmail.com>
2025-10-14 16:50:57 +0900

    chore: 초기 세팅(pnpm, 컬러/ 폰트 스타일)

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <main className="font-pretendard">
+      <h2 className="text-4xl font-bold">Coworkers</h2>
+      <h2 className="text-3xl font-semibold">Coworkers</h2>
+      <h2 className="text-2xl font-medium">Coworkers</h2>
+      <h2 className="text-xl">Coworkers</h2>
+      <br />
+      <h2 className="text-4xl font-bold text-primary">Coworkers</h2>
+      <h2 className="text-3xl font-semibold text-secondary">Coworkers</h2>
+      <h2 className="text-2xl font-medium text-tertiary">Coworkers</h2>
+      <h2 className="text-xl text-disabled">Coworkers</h2>
+      <br />
+      <h2 className="text-4xl text-text-primary">Coworkers</h2>
+      <h2 className="text-3xl text-text-secondary">Coworkers</h2>
+      <h2 className="text-2xl text-text-tertiary">Coworkers</h2>
+      <h2 className="text-xl text-text-default">Coworkers</h2>
+    </main>
+  );
+}
+
+export default App;
