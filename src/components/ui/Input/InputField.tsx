335f63eced096e203c6a6c5dd1710e4072c7962f
leesangdal <asksa1256@gmail.com>
2025-10-16 16:55:33 +0900

    feat: PasswordField 컴포넌트 생성

--- src/components/ui/Input/InputField.tsx
+++ src/components/ui/Input/InputField.tsx
@@ -1,22 +1,34 @@
+import { cn } from '@/lib/utils';
 import { type FieldError } from '@/types/types';
 import Input from '.';
 
 interface InputFieldProps extends React.ComponentProps<'input'> {
   error?: FieldError | null;
+  suffix?: React.ReactNode; // 우측 버튼 요소
 }
 
 export default function InputField({
   className,
   type,
+  suffix,
   error,
   ...props
 }: InputFieldProps) {
   return (
-    <div className={className}>
-      <Input type={type} error={error} {...props} />
+    <>
+      <div className={cn('relative', className)}>
+        <Input type={type} error={error} {...props} />
+
+        {suffix && (
+          <div className="absolute inset-y-0 right-2 z-[1] flex items-center">
+            {suffix}
+          </div>
+        )}
+      </div>
+
       {error && (
         <p className="text-md text-destructive mt-2">{error.message}</p>
       )}
-    </div>
+    </>
   );
 }
