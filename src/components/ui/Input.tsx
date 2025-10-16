511691658c50f1817f2b4de34ccb83b6dcf00ab8
leesangdal <asksa1256@gmail.com>
2025-10-16 15:05:30 +0900

    feat: Input 공용 컴포넌트 생성

---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+
+import { cn } from '@/lib/utils';
+
+interface InputProps extends React.ComponentProps<'input'> {
+  isError: boolean;
+}
+
+function Input({ className, type, isError = false, ...props }: InputProps) {
+  return (
+    <input
+      type={type}
+      data-slot="input"
+      className={cn(
+        'text-md text-text-primary w-full min-w-0 rounded-[12px] border border-[#e2e8f0] bg-transparent px-4 py-[12px] transition-colors outline-none md:px-4 md:py-[13px] md:text-base',
+        'placeholder:text-text-default selection:text-text-primary',
+        'hover:border-primary focus-visible:border-primary-pressed',
+        'disabled:bg-bg-secondary disabled:text-disabled disabled:pointer-events-none disabled:cursor-not-allowed',
+        'aria-invalid:ring-destructive/20 aria-invalid:border-destructive',
+        'dark:bg-input/30 dark:aria-invalid:ring-destructive/40',
+        'file:text-foreground file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium',
+        { 'border-destructive': isError },
+        className,
+      )}
+      {...props}
+    />
+  );
+}
+
+export default Input;
