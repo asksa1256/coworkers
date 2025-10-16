a92d7bdd6a44cbce2f2f405bfbbe99c689461782
leesangdal <asksa1256@gmail.com>
2025-10-16 18:27:04 +0900

    feat: InputReply(댓글 입력) 컴포넌트 생성

---
+++ src/components/feature/comments/InputReply.tsx
@@ -0,0 +1,46 @@
+import Button from '@/components/ui/Button';
+import { cn } from '@/lib/utils';
+import { type FieldError } from '@/types/types';
+import { useRef } from 'react';
+
+interface InputReplyProps extends React.ComponentProps<'textarea'> {
+  name: string; // 폼 제출용 필드
+  error?: FieldError | null;
+  disabled: boolean;
+  className?: string;
+}
+
+export default function InputReply({
+  name,
+  error,
+  disabled = false,
+  className,
+}: InputReplyProps) {
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+
+  const handleInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const el = e.target;
+    el.style.height = 'auto';
+    el.style.height = `${el.scrollHeight}px`;
+  };
+
+  return (
+    <div className={cn('flex w-full flex-col gap-1', className)}>
+      <div className="border-border flex w-full gap-6 border-t border-b px-3 py-3">
+        <textarea
+          ref={textareaRef}
+          name={name}
+          placeholder="댓글을 달아주세요."
+          onInput={handleInput}
+          className="md:text-md w-full resize-none pt-1 text-xs md:pt-0.5"
+          rows={1}
+        ></textarea>
+        <Button size="icon-md" round="full" disabled={disabled}>
+          <img src="/icons/ArrowUpIcon.svg" alt="댓글 달기" />
+        </Button>
+      </div>
+
+      {error && <p className="text-destructive text-sm">{error.message}</p>}
+    </div>
+  );
+}
---
+++ src/stories/InputReply.stories.tsx
@@ -0,0 +1,44 @@
+import InputReply from '@/components/feature/comments/InputReply';
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { fn } from 'storybook/test';
+
+const meta = {
+  title: 'Example/InputReply',
+  component: InputReply,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  args: { onClick: fn() },
+  argTypes: {
+    disabled: {
+      control: 'boolean',
+      description: '인풋 비활성화 여부',
+    },
+    error: {
+      control: 'object',
+      description: '유효성 검사 실패 시 전달되는 에러 객체 (message 필드 포함)',
+    },
+    placeholder: { control: 'text' },
+  },
+} satisfies Meta<typeof InputReply>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    placeholder: '이름을 입력하세요',
+    error: null,
+  },
+};
+
+export const WithError: Story = {
+  args: {
+    error: {
+      message: '유효한 이메일이 아닙니다.',
+    },
+    value: 'codeit@code.com',
+  },
+  name: '에러 메시지 표시',
+};
