c2ed63d7b7697a91921047e2ee70a98ab94f5932
taegyung <taegyung.e@gmail.com>
2025-10-17 18:16:18 +0900

    feat: 공용 레이아웃 작업 완료

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,10 +1,11 @@
+import Header from '@/components/layout/Header';
 import { Outlet } from 'react-router-dom';
 
 export default function Layout() {
   return (
-    <div>
-      <header>헤더</header>
-      <main>
+    <div className="mx-auto flex min-h-screen max-w-[1920px] flex-col md:flex-row">
+      <Header />
+      <main className="border-border-primary grow-1 border-t px-4 has-[.bt-none]:border-none md:border-none md:px-[26px] lg:px-[30px]">
         <Outlet />
       </main>
     </div>
