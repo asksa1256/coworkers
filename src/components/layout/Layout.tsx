4f9cc526d13f8fde84e898c98225529a4175b23a
taegyung <taegyung.e@gmail.com>
2025-10-17 22:56:44 +0900

    chore: main 영역 자식 요소의 높이도 풀로 채울수 있게 flex 설정

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -5,7 +5,7 @@ export default function Layout() {
   return (
     <div className='mx-auto flex min-h-screen max-w-[1920px] flex-col md:flex-row'>
       <Header />
-      <main className='grow-1 px-4 md:px-[26px] lg:px-[30px]'>
+      <main className='flex grow-1 flex-col px-4 md:px-[26px] lg:px-[30px]'>
         <Outlet />
       </main>
     </div>
