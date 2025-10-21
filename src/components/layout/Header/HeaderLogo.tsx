f09056bb60918fc4c0de8f4a4123a15f57447c7e
taegyung <taegyung.e@gmail.com>
2025-10-22 00:17:04 +0900

    chore: Logo 접근성 aria-label 추가

--- src/components/layout/Header/HeaderLogo.tsx
+++ src/components/layout/Header/HeaderLogo.tsx
@@ -10,8 +10,16 @@ interface Props {
 export default function HeaderLogo({ iconOnly }: Props) {
   return (
     <Link to='/' className='text-primary flex h-full gap-0.5'>
-      <LogoIcon className={cn(`w-4 md:w-6`, iconOnly && 'w-6')} />
-      {!iconOnly && <LogoText className='w-[84px] md:w-[132px]' />}
+      <LogoIcon
+        className={cn(`w-4 md:w-6`, iconOnly && 'w-6')}
+        aria-label='COWORKERS 로고 아이콘'
+      />
+      {!iconOnly && (
+        <LogoText
+          className='w-[84px] md:w-[132px]'
+          aria-label='COWORKERS 로고 텍스트'
+        />
+      )}
     </Link>
   );
 }
