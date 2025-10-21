d377ee31150441bdc21a4bc265e1b9824b8e4361
leesangdal <asksa1256@gmail.com>
2025-10-21 12:34:05 +0900

    fix: 로그인 폼 스키마 - 이메일 입력값 없을 시 '이메일 필수 입력' 메시지부터 나타나게 수정

--- src/types/SignInSchema.ts
+++ src/types/SignInSchema.ts
@@ -2,11 +2,12 @@ import { z } from 'zod';
 
 export const signInSchema = z.object({
   email: z
-    .email('이메일 형식으로 작성해주세요.')
-    .min(1, '이메일을 입력해주세요.'),
+    .string()
+    .min(1, '이메일은 필수 입력입니다.')
+    .and(z.email('이메일 형식으로 작성해주세요.')),
   password: z
     .string()
-    .min(1, '비밀번호를 입력해주세요.')
+    .min(1, '비밀번호는 필수 입력입니다.')
     .min(8, '비밀번호는 최소 8자 이상이어야 합니다.'),
 });
 
