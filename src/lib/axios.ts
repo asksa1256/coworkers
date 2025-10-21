059acf53e93bd7c3e834b853493c2f7c40e1c594
leesangdal <asksa1256@gmail.com>
2025-10-21 19:18:07 +0900

    fix: axiosInstance - 환경변수 import 방식 수정

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL, // vite는 process 대신 import.meta 사용
   timeout: 10000, // 서버 응답 최대 대기 시간 10초
   headers: {
     'Content-Type': 'application/json',
