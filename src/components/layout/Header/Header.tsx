ef4a1abe94766ad1f74ad9a9e58e4f54be524332
taegyung <taegyung.e@gmail.com>
2025-10-21 14:14:03 +0900

    chore: 스웨거 기반 유저 정보 전달되도록 수정

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,18 +1,84 @@
 import MobileHeader from '@/components/layout/Header/MobileHeader';
 import SidebarHeader from '@/components/layout/Header/SidebarHeader';
 import useCurrentView from '@/hooks/useCurrentView';
+import type { MembershipsType, UserType } from '@/types/userType';
 import { useState } from 'react';
 
+const TEST_USER: UserType = {
+  id: 2318,
+  nickname: 'aaa1111',
+  createdAt: '2025-10-21T01:01:25+09:00',
+  updatedAt: '2025-10-21T01:01:25+09:00',
+  image: null,
+  teamId: '16-16',
+  email: 'aaa1111@aaaa.com',
+  memberships: [
+    {
+      userId: 2318,
+      groupId: 3244,
+      userName: 'aaa1111',
+      userEmail: 'aaa1111@aaaa.com',
+      userImage: null,
+      role: 'ADMIN',
+      group: {
+        id: 3244,
+        name: '스누피처럼살자',
+        image:
+          'https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Coworkers/user/2318/images.jpg',
+        createdAt: '2025-10-21T01:05:40+09:00',
+        updatedAt: '2025-10-21T01:05:40+09:00',
+        teamId: '16-16',
+      },
+    },
+    {
+      userId: 2318,
+      groupId: 3245,
+      userName: 'aaa1111',
+      userEmail: 'aaa1111@aaaa.com',
+      userImage: null,
+      role: 'MEMBER',
+      group: {
+        id: 3245,
+        name: '도비는자유에요',
+        image:
+          'https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Coworkers/user/2319/dobbyisfree.jpg',
+        createdAt: '2025-10-21T01:20:24+09:00',
+        updatedAt: '2025-10-21T01:20:24+09:00',
+        teamId: '16-16',
+      },
+    },
+  ],
+};
+
 export default function Header() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [currentGroup, setCurrentGroup] = useState<MembershipsType | null>(
+    null,
+  );
   const currentView = useCurrentView();
+  const handleUpdateCurrentGroup = (group: MembershipsType) => {
+    setCurrentGroup(group);
+  };
+
+  // 로그인 후 jotai를 통해 유저 정보 전역에서 관리할 수 있게 처리
+  // 로그인 작업 후에 적용 예정, 현재 프롭스드릴링으로 적용
 
   return (
     <>
       {currentView === 'MOBILE' ? (
-        <MobileHeader isLoggedIn={isLoggedIn} />
+        <MobileHeader
+          isLoggedIn={isLoggedIn}
+          user={TEST_USER}
+          currentGroup={currentGroup}
+          onUpdateCurrentGroup={handleUpdateCurrentGroup}
+        />
       ) : (
-        <SidebarHeader isLoggedIn={isLoggedIn} />
+        <SidebarHeader
+          isLoggedIn={isLoggedIn}
+          user={TEST_USER}
+          currentGroup={currentGroup}
+          onUpdateCurrentGroup={handleUpdateCurrentGroup}
+        />
       )}
     </>
   );
--- src/components/layout/Header/SidebarHeader/index.tsx
+++ src/components/layout/Header/SidebarHeader/index.tsx
@@ -1,23 +1,83 @@
+import DefaultProfileIcon from '@/assets/icons/DefaultProfileIcon.svg?react';
 import GnbFoldIcon from '@/assets/icons/GnbFoldIcon.svg?react';
+import HeaderGnb from '@/components/layout/Header/HeaderGnb';
 import HeaderLogo from '@/components/layout/Header/HeaderLogo';
+import SidebarUser from '@/components/layout/Header/SidebarHeader/SidebarUser';
+import { cn } from '@/lib/utils';
+import type { MembershipsType, UserType } from '@/types/userType';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 interface Props {
   isLoggedIn: boolean;
+  user: UserType;
+  currentGroup: MembershipsType | null;
+  onUpdateCurrentGroup: (group: MembershipsType) => void;
 }
 
-export default function SidebarHeader({ isLoggedIn }: Props) {
+export default function SidebarHeader({
+  isLoggedIn,
+  user,
+  currentGroup,
+  onUpdateCurrentGroup,
+}: Props) {
   const [isFold, setIsFold] = useState(false);
 
   return (
-    <header className='bg-bg-primary border-border-primary sticky top-0 h-screen w-[270px] border-r'>
-      <div>
-        <HeaderLogo />
-        <button>
-          <GnbFoldIcon />
+    <header
+      className={cn(
+        'bg-bg-primary border-border-primary group sticky top-0 flex h-screen w-[270px] flex-col border-r transition-all',
+        isFold && 'is-fold w-[72px]',
+      )}
+    >
+      {/* 상단 영역 */}
+      <div
+        className={cn(
+          'relative flex shrink-0 items-center justify-between px-[26px] py-8',
+          'group-[.is-fold]:px-6',
+        )}
+      >
+        <HeaderLogo iconOnly={isFold} />
+        <button
+          className='group-[.is-fold]:bg-bg-primary group-[.is-fold]:border-border-primary group-[.is-fold]:absolute group-[.is-fold]:top-1/2 group-[.is-fold]:right-0 group-[.is-fold]:h-8 group-[.is-fold]:w-8 group-[.is-fold]:translate-x-[18px] group-[.is-fold]:-translate-y-1/2 group-[.is-fold]:rounded-full group-[.is-fold]:border'
+          onClick={() => {
+            setIsFold(prev => !prev);
+          }}
+        >
+          <GnbFoldIcon className='mx-auto group-[.is-fold]:w-6 group-[.is-fold]:rotate-180' />
         </button>
       </div>
-      <div></div>
+      {/* 중간 영역 */}
+      {isLoggedIn && (
+        <div
+          className={cn(
+            'mt-6 flex grow-1 flex-col overflow-hidden px-4',
+            'group-[.is-fold]:px-[10px]',
+          )}
+        >
+          <HeaderGnb
+            user={user}
+            currentGroup={currentGroup}
+            onUpdateCurrentGroup={onUpdateCurrentGroup}
+          />
+        </div>
+      )}
+      {/* 하단 영역 */}
+      <div className='mt-auto shrink-0'>
+        {isLoggedIn ? (
+          <SidebarUser user={user} currentGroup={currentGroup} />
+        ) : (
+          <Link
+            className='border-border-primary mx-4 flex items-center gap-3 border-t pt-5 pb-6 leading-10 font-medium'
+            to='/login'
+          >
+            <span className='flex h-10 w-10 items-center justify-center rounded-xl bg-slate-200'>
+              <DefaultProfileIcon />
+            </span>
+            로그인
+          </Link>
+        )}
+      </div>
     </header>
   );
 }
