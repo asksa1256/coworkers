1c9e9a172368d4f4b7a29bcf8a8c23b06d79ce6a
leesangdal <asksa1256@gmail.com>
2025-10-21 18:48:35 +0900

    refactor: axiosInstance baseUrl 환경변수화

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://fe-project-cowokers.vercel.app/16-16',
+  baseURL: process.env.VITE_API_BASE_URL,
   timeout: 10000, // 서버 응답 최대 대기 시간 10초
   headers: {
     'Content-Type': 'application/json',
