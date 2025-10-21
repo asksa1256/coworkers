88a843cf642bc11a5abee4b77bc3ec2db075b9ce
leesangdal <asksa1256@gmail.com>
2025-10-21 20:47:42 +0900

    feat: routes에 권한별 리디렉션 적용

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,13 @@
+import AuthRoute from '@/components/feature/auth/AuthRoute';
+import PrivateRoute from '@/components/feature/auth/PrivateRoute';
 import Layout from '@/components/layout/Layout';
 import ErrorPage from '@/pages/ErrorPage';
 import LandingPage from '@/pages/LandingPage';
 import ListPage from '@/pages/ListPage';
 import TeamPage from '@/pages/TeamPage';
-import SignInPage from '@/pages/auth/SignInPage'
+import SignInPage from '@/pages/auth/SignInPage';
+import BoardPage from '@/pages/board/BoardPage';
+import PostDetailPage from './pages/board/PostDetailPage';
 
 const routes = [
   {
@@ -12,18 +16,58 @@ const routes = [
     errorElement: <ErrorPage />,
     children: [
       { path: '/', element: <LandingPage /> },
-      { path: '/list/:groupId', element: <ListPage /> },
-      { path: '/:teamId', element: <TeamPage /> },
+      {
+        path: '/list/:groupId',
+        element: (
+          <PrivateRoute>
+            <ListPage />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: '/:teamId',
+        element: (
+          <PrivateRoute>
+            <TeamPage />
+          </PrivateRoute>
+        ),
+      },
     ],
   },
   {
     path: '/auth',
-    element: <Layout/>,
+    element: <Layout />,
     errorElement: <ErrorPage />,
     children: [
-      {path: 'signin', element: <SignInPage/>}
-    ]
-  }
+      {
+        path: 'signin',
+        element: (
+          <AuthRoute>
+            <SignInPage />
+          </AuthRoute>
+        ),
+      },
+    ],
+  },
+  {
+    path: '/board',
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <BoardPage />,
+      },
+      {
+        path: ':postId',
+        element: (
+          <PrivateRoute>
+            <PostDetailPage />
+          </PrivateRoute>
+        ),
+      },
+    ],
+  },
 ];
 
 export default routes;
