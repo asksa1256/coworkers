ef25d2e51479358c203f3b199ae5c97be52cdba5
leesangdal <asksa1256@gmail.com>
2025-10-21 20:50:58 +0900

    feat: 로그아웃 훅 - 로그아웃 시 로그인 페이지 리디렉션 코드 제거

--- src/hooks/useSignOut.ts
+++ src/hooks/useSignOut.ts
@@ -1,16 +1,13 @@
 import { userAtom } from '@/store/authAtom';
 import { removeTokens } from '@/utils/tokenStorage';
 import { useSetAtom } from 'jotai';
-import { useNavigate } from 'react-router-dom';
 
 const useSignOut = () => {
   const setUser = useSetAtom(userAtom);
-  const navigate = useNavigate();
 
   const signOut = () => {
     removeTokens();
     setUser(null);
-    navigate('/auth/signin');
   };
 
   return signOut;
