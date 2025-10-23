61da4f6e756d735eaa11e06428769f5500534181
leesangdal <asksa1256@gmail.com>
2025-10-23 19:22:52 +0900

    fix: EditableAvatar 기본 이미지일 경우 배경색 수정

--- src/components/feature/profile/EditableAvatar.tsx
+++ src/components/feature/profile/EditableAvatar.tsx
@@ -17,7 +17,7 @@ export default function EditableAvatar({
       <Avatar
         imgSrc={imgSrc}
         className={cn(
-          'border-border-primary bg-bg-tertiary/30 relative h-16 w-16 cursor-pointer rounded-[20px] border-2 transition-colors md:h-[100px] md:w-[100px] md:rounded-4xl',
+          'border-border-primary bg-bg-tertiary/50 relative h-16 w-16 cursor-pointer rounded-[20px] border-2 transition-colors md:h-[100px] md:w-[100px] md:rounded-4xl',
           'group-hover:bg-bg-tertiary',
           !imgSrc && '[&_img]:size-12 md:[&_img]:size-20',
           imgSrc &&
