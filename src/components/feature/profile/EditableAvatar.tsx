3444a78d71fb15747b5854d420b104a63da8c670
leesangdal <asksa1256@gmail.com>
2025-10-23 19:17:45 +0900

    style: EditableAvatar 공용 컴포넌트 퍼블리싱

--- src/components/feature/profile/EditableAvatar.tsx
+++ src/components/feature/profile/EditableAvatar.tsx
@@ -1,5 +1,36 @@
+import PencilIcon from '@/assets/icons/PencilIcon.svg?react';
 import Avatar from '@/components/ui/Avatar';
+import Button from '@/components/ui/Button';
+import { cn } from '@/lib/utils';
 
-export default function EditableAvatar() {
-  return <Avatar imgSrc={null} />;
+interface EditableAvatarProps {
+  imgSrc: string | null;
+  className?: string;
+}
+
+export default function EditableAvatar({
+  imgSrc,
+  className,
+}: EditableAvatarProps) {
+  return (
+    <div className={cn('group relative transition-colors', className)}>
+      <Avatar
+        imgSrc={imgSrc}
+        className={cn(
+          'border-border-primary bg-bg-tertiary/30 relative h-16 w-16 cursor-pointer rounded-[20px] border-2 transition-colors md:h-[100px] md:w-[100px] md:rounded-4xl',
+          'group-hover:bg-bg-tertiary',
+          'before:absolute before:inset-0 before:rounded-[18px] before:bg-black/0 before:transition-colors group-hover:before:bg-black/30 md:before:rounded-[30px]',
+          !imgSrc && '[&_img]:size-12 md:[&_img]:size-20',
+        )}
+      />
+
+      <Button
+        size='icon-lg'
+        round='full'
+        className='bg-bg-tertiary group-hover:bg-primary hover:bg-primary absolute -right-1 bottom-0'
+      >
+        <PencilIcon className='text-icon-primary group-hover:text-white group-focus:text-white' />
+      </Button>
+    </div>
+  );
 }
---
+++ src/stories/EditableAvatar.stories.tsx
@@ -0,0 +1,28 @@
+import DefaultProfileIcon from '@/assets/icons/DefaultProfileIcon.svg';
+import EditableAvatar from '@/components/feature/profile/EditableAvatar';
+import type { Meta, StoryObj } from '@storybook/react-vite';
+
+const meta = {
+  title: 'Example/Avatar/EditableAvatar',
+  component: EditableAvatar,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof EditableAvatar>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    imgSrc: DefaultProfileIcon,
+  },
+};
+
+export const EditableAvatarWithImage: Story = {
+  args: {
+    imgSrc:
+      'https://thumbnail.coupangcdn.com/thumbnails/remote/492x492ex/image/vendor_inventory/2d1c/7ad2c762d37fbd312d08861c34db742f14091a3cb9753ce415da7e4b0413.jpg',
+  },
+};
