32ed67b91a5a6f2b069151c800979395cb29ad59
taegyung <taegyung.e@gmail.com>
2025-10-23 22:38:46 +0900

    style: 모달 css 수정

--- src/components/ui/Modal/index.tsx
+++ src/components/ui/Modal/index.tsx
@@ -20,10 +20,13 @@ const modalStyle = cva(
       mode: {
         bottomSheet: `
           top-[auto] bottom-0 translate-y-full rounded-none p-12 pb-8
+          data-[state=open]:zoom-in-100 data-[state=open]:slide-in-from-bottom data-[state=open]:translate-y-0
+
           md:top-1/2 md:bottom-[auto] md:w-[384px] md:-translate-y-1/2 md:px-[52px] md:pt-12 md:pb-8
+          md:data-[state=open]:zoom-in-95 md:data-[state=open]:animate-in md:data-[state=open]:slide-in-from-bottom-0 md:data-[state=open]:-translate-y-1/2
         `,
         normal: `
-          sm:max-w-[90vw] w-[90%] rounded-xl py-8 px-6 shadow-[4px_4px_10px_rgba(36,36,36,0.25)
+          sm:max-w-[90vw] w-[90%] rounded-xl py-8 px-6 shadow-[4px_4px_10px_rgba(36,36,36,0.25)]
           md:w-[384px]
         `,
       },
@@ -31,19 +34,11 @@ const modalStyle = cva(
         sm: 'rounded-t-xl md:rounded-xl',
         lg: 'rounded-t-3xl md:rounded-3xl',
       },
-      motion: {
-        bottomSheet: `
-          data-[state=open]:zoom-in-100 data-[state=open]:slide-in-from-bottom data-[state=open]:translate-y-0
-          md:data-[state=open]:zoom-in-95 md:data-[state=open]:animate-in md:data-[state=open]:slide-in-from-bottom-0 md:data-[state=open]:-translate-y-1/2
-        `,
-        normal: ``,
-      },
     },
     compoundVariants: [{ mode: 'normal', className: 'rounded-3xl' }],
     defaultVariants: {
       mode: 'bottomSheet',
       round: 'lg',
-      motion: 'bottomSheet',
     },
   },
 );
@@ -54,13 +49,7 @@ export default function Modal() {
   const { isOpen, options } = modal;
 
   if (options === null) return;
-  const {
-    children,
-    closeIconButton,
-    className,
-    mode = 'bottomSheet',
-    round,
-  } = options;
+  const { children, closeIconButton, className, mode, round } = options;
 
   return (
     <Dialog open={isOpen} onOpenChange={closeModal}>
@@ -70,7 +59,6 @@ export default function Modal() {
           modalStyle({
             mode,
             round,
-            motion: mode,
           }),
           className,
         )}
