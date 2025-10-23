3ef52e41fa71a4ba689bcb2a5da6de31c73bb9fc
taegyung <taegyung.e@gmail.com>
2025-10-23 15:07:27 +0900

    feat: 공용 컴포넌트 모달 제작

--- src/components/ui/Modal/index.tsx
+++ src/components/ui/Modal/index.tsx
@@ -20,13 +20,10 @@ const modalStyle = cva(
       mode: {
         bottomSheet: `
           top-[auto] bottom-0 translate-y-full rounded-none p-12 pb-8
-          data-[state=open]:zoom-in-100 data-[state=open]:slide-in-from-bottom data-[state=open]:translate-y-0
-
           md:top-1/2 md:bottom-[auto] md:w-[384px] md:-translate-y-1/2 md:px-[52px] md:pt-12 md:pb-8
-          md:data-[state=open]:zoom-in-95 md:data-[state=open]:animate-in md:data-[state=open]:slide-in-from-bottom-0 md:data-[state=open]:-translate-y-1/2
         `,
         normal: `
-          sm:max-w-[90vw] w-[90%] rounded-xl py-8 px-6 shadow-[4px_4px_10px_rgba(36,36,36,0.25)]
+          sm:max-w-[90vw] w-[90%] rounded-xl py-8 px-6 shadow-[4px_4px_10px_rgba(36,36,36,0.25)
           md:w-[384px]
         `,
       },
@@ -34,11 +31,19 @@ const modalStyle = cva(
         sm: 'rounded-t-xl md:rounded-xl',
         lg: 'rounded-t-3xl md:rounded-3xl',
       },
+      motion: {
+        bottomSheet: `
+          data-[state=open]:zoom-in-100 data-[state=open]:slide-in-from-bottom data-[state=open]:translate-y-0
+          md:data-[state=open]:zoom-in-95 md:data-[state=open]:animate-in md:data-[state=open]:slide-in-from-bottom-0 md:data-[state=open]:-translate-y-1/2
+        `,
+        normal: ``,
+      },
     },
     compoundVariants: [{ mode: 'normal', className: 'rounded-3xl' }],
     defaultVariants: {
       mode: 'bottomSheet',
       round: 'lg',
+      motion: 'bottomSheet',
     },
   },
 );
@@ -59,6 +64,7 @@ export default function Modal() {
           modalStyle({
             mode,
             round,
+            motion: mode,
           }),
           className,
         )}
