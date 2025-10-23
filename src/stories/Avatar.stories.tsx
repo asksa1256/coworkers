57085456f7941cccb0785ffebd5acfe1e6f9684d
leesangdal <asksa1256@gmail.com>
2025-10-23 18:29:38 +0900

    feat: Avatar 스토리 - 기본 이미지 / 이미지 있는 예시 추가 + Avatar 그룹화

--- src/stories/Avatar.stories.tsx
+++ src/stories/Avatar.stories.tsx
@@ -1,9 +1,8 @@
-import DefaultProfileIcon from '@/assets/icons/DefaultProfileIcon.svg';
 import Avatar from '@/components/ui/Avatar';
 import type { Meta, StoryObj } from '@storybook/react-vite';
 
 const meta = {
-  title: 'Example/Avatar',
+  title: 'Example/Avatar/Avatar',
   component: Avatar,
   parameters: {
     layout: 'centered',
@@ -16,6 +15,13 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    imgSrc: DefaultProfileIcon,
+    imgSrc: null,
+  },
+};
+
+export const AvatarWithImage: Story = {
+  args: {
+    imgSrc:
+      'https://thumbnail.coupangcdn.com/thumbnails/remote/492x492ex/image/vendor_inventory/2d1c/7ad2c762d37fbd312d08861c34db742f14091a3cb9753ce415da7e4b0413.jpg',
   },
 };
