eafe0dc1d94bf0eb239b3f85d91a77c083196439
tasi <taegyung.e@gmail.com>
2025-11-01 00:59:30 +0900

    feat: 팀 참여하기 기능 구현

--- src/pages/JoinTeamPage.tsx
+++ src/pages/JoinTeamPage.tsx
@@ -1,34 +1,97 @@
+import { getGroup } from '@/api/api';
+import Avatar from '@/components/ui/Avatar';
 import Button from '@/components/ui/Button';
-import InputField from '@/components/ui/Input/InputField';
-import { Label } from '@/components/ui/Label';
+import axiosInstance from '@/lib/axios';
+import { userAtom } from '@/store/authAtom';
+import type { GroupDetailResponse } from '@/types/groupType';
+import type { AxiosError } from 'axios';
+import { useAtomValue } from 'jotai';
+import { UserRound } from 'lucide-react';
+import { useEffect, useState } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { toast } from 'sonner';
 
 export default function JoinTeamPage() {
+  const navigate = useNavigate();
+  const searchParams = useSearchParams()[0];
+  const token = searchParams.get('token');
+  const groupId = searchParams.get('groupId');
+  const [groupInfo, setGroupInfo] = useState<GroupDetailResponse | null>(null);
+  const user = useAtomValue(userAtom);
+
+  useEffect(() => {
+    if (!token || !groupId) {
+      toast.error('잘못된 초대 링크입니다. 메인 페이지로 이동합니다.');
+      navigate('/', { replace: true });
+      return;
+    }
+
+    const getInfo = async () => {
+      try {
+        const groupData = await getGroup<GroupDetailResponse>(Number(groupId));
+        setGroupInfo(groupData);
+      } catch (error) {
+        console.error(error);
+        toast.error('팀 정보를 불러오지 못했습니다.');
+      }
+    };
+    getInfo();
+  }, [token, groupId, navigate]);
+
+  const handleClickJoin = async () => {
+    if (!user) return;
+
+    try {
+      const payload = {
+        userEmail: user.email,
+        token: token,
+      };
+
+      const { data } = await axiosInstance.post(
+        '/groups/accept-invitation',
+        payload,
+      );
+
+      toast.success('팀 참여하기 성공! 팀페이지로 이동합니다.');
+      navigate(`/${data.groupId}`);
+    } catch (error) {
+      const err = error as AxiosError<{ message: string }>;
+      const message =
+        err.response?.data?.message ?? '알 수 없는 에러가 발생했습니다.';
+      toast.error(message);
+    }
+  };
+
   return (
     <div className='my-5 flex grow-1 flex-col justify-center md:my-10'>
-      <form className='bg-bg-primary mx-auto w-full max-w-[550px] rounded-[20px] px-5 pt-[46px] pb-[60px] md:px-[45px] md:pt-[60px] md:pb-[62px]'>
+      <div className='bg-bg-primary mx-auto w-full max-w-[550px] rounded-[20px] px-5 pt-[46px] pb-[60px] md:px-[45px] md:pt-[60px] md:pb-[62px]'>
         <h2 className='text-xl font-bold md:text-2xl'>팀 참여하기</h2>
 
-        <div className='mt-[22px] md:mt-12'>
-          <Label
-            htmlFor='name'
-            className='text-md mb-2 block font-medium md:text-base'
-          >
-            팀 링크
-          </Label>
-          <InputField
-            id='name'
-            type='text'
-            placeholder='팀 링크를 입력해주세요.'
+        <div className='mt-5 text-center'>
+          <Avatar
+            imgSrc={groupInfo?.image || null}
+            size='lg'
+            className='h-16 w-16 rounded-full! md:h-[100px] md:w-[100px] lg:h-[100px] lg:w-[100px]'
           />
+          <h3 className='mt-2 text-lg font-bold md:text-xl'>
+            {groupInfo?.name}
+          </h3>
+          <span className='text-text-default text-md mt-1 flex items-center justify-center gap-1'>
+            <UserRound className='w-4' /> {groupInfo?.members.length ?? 1}명
+          </span>
         </div>
 
-        <Button type='submit' className='mt-10 text-base'>
+        <Button
+          type='button'
+          className='mt-10 text-base'
+          onClick={handleClickJoin}
+        >
           참여하기
         </Button>
         <p className='text-text-default mt-5 text-center text-xs md:mt-6 md:text-base'>
-          공유받은 팀 링크를 입력해 참여할 수 있어요.
+          {`참여를 원하시면, '참여하기' 버튼을 클릭해주세요.`}
         </p>
-      </form>
+      </div>
     </div>
   );
 }
--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -1,7 +1,70 @@
+import Button from '@/components/ui/Button';
+import axiosInstance from '@/lib/axios';
+import { isAxiosError } from 'axios';
+import { useParams } from 'react-router-dom';
+import { toast } from 'sonner';
+
 export default function ListPage() {
+  const { groupId } = useParams();
+
+  const handleCopyInviteLink = async () => {
+    if (!groupId) {
+      toast.error('팀 정보가 없습니다.');
+      return;
+    }
+
+    try {
+      const { data: token } = await axiosInstance.get(
+        `/groups/${groupId}/invitation`,
+      );
+
+      const origin = window.location.origin;
+      const url = new URL('/join-team', origin);
+      url.searchParams.set('token', token);
+      url.searchParams.set('groupId', groupId);
+
+      await copyToClipboard(url.toString());
+      toast.success('초대 링크가 복사되었습니다!');
+    } catch (error) {
+      if (isAxiosError(error)) {
+        // 엔드포인트 /groups/${groupId}/invitation의 에러 처리
+        toast.error('초대 링크를 생성하지 못했습니다.');
+      } else {
+        // copyToClipboard의 에러 처리
+        toast.error('복사에 실패했습니다. 다시 시도해주세요.');
+      }
+      console.error(error);
+    }
+  };
+
+  const copyToClipboard = async (text: string) => {
+    if (navigator.clipboard) {
+      // navigator.clipboard 지원O
+      await navigator.clipboard.writeText(text);
+    } else {
+      // navigator.clipboard 지원X - fallback용
+      // ex) iphone safari, IE ...
+      const textArea = document.createElement('textarea');
+      textArea.value = text;
+      textArea.style.position = 'fixed';
+      textArea.style.opacity = '0';
+      textArea.style.zIndex = '-9999';
+      document.body.appendChild(textArea);
+      textArea.focus();
+      textArea.select();
+
+      const result = document.execCommand('copy');
+      document.body.removeChild(textArea);
+
+      if (!result) throw new Error('execCommand 복사 실패');
+    }
+  };
+
   return (
     <>
-      <div className='h-full'>리스트 페이지</div>
+      <div className='h-full'>
+        <Button onClick={handleCopyInviteLink}>링크 복사하기</Button>
+      </div>
     </>
   );
 }
