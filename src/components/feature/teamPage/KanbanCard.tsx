a6bbda515e60d9571ef59187a207435a455acd6c
unknown <zxcv530@naver.com>
2025-11-01 02:02:23 +0900

    feat: TaskCheckbox 토글 낙관적 업데이트 구현

--- src/components/feature/teamPage/KanbanCard.tsx
+++ src/components/feature/teamPage/KanbanCard.tsx
@@ -1,9 +1,12 @@
+import { updateTask } from '@/api/api';
 import KebabIcon from '@/assets/icons/KebabIcon.svg?react';
 import CircularProgressbar from '@/components/ui/CircularProgressbar';
 import TaskCheckbox from '@/components/ui/TaskCheckbox';
 import { cn } from '@/lib/utils';
+import type { GroupDetailResponse } from '@/types/groupType';
 import type { TaskListsResponse } from '@/types/taskType';
 import { countDone } from '@/utils/calculations';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import clsx from 'clsx';
 import { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
@@ -15,8 +18,56 @@ interface Props {
 }
 
 export default function KanbanCard({ taskList, tab, onDragStart }: Props) {
+  const queryClient = useQueryClient();
   const [isDragging, setIsDragging] = useState(false);
   const { pathname } = useLocation();
+  const groupId = Number(pathname.slice(1));
+  const taskDoneMutation = useMutation({
+    mutationFn: (args: Parameters<typeof updateTask>) => updateTask(...args),
+    onMutate: async variables => {
+      await queryClient.cancelQueries({
+        queryKey: ['group', groupId],
+      });
+
+      const prevSnapshot = queryClient.getQueryData(['group', groupId]);
+
+      queryClient.setQueryData(
+        ['group', groupId],
+        (prev: GroupDetailResponse) => {
+          const taskId = variables[0];
+          const done = variables[1].done;
+
+          const updatedTasks = taskList.tasks.map(task => {
+            return task.id === taskId
+              ? {
+                  ...task,
+                  doneAt: done ? 'temp value' : null,
+                }
+              : task;
+          });
+
+          const updatedLists = prev.taskLists.map(prevList => {
+            return prevList.id === taskList.id
+              ? { ...prevList, tasks: updatedTasks }
+              : prevList;
+          });
+
+          return {
+            ...prev,
+            taskLists: updatedLists,
+          };
+        },
+      );
+
+      return { prevSnapshot };
+    },
+    onError: (error, variables, context) => {
+      queryClient.setQueryData(['group', groupId], context?.prevSnapshot);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ['group', groupId] });
+    },
+  });
 
   /**
    * dataTransfer은 현재 이동중인 카드 id와 속한 탭을 dragovder, drop이벤트에 전달
@@ -28,13 +79,8 @@ export default function KanbanCard({ taskList, tab, onDragStart }: Props) {
     setIsDragging(true);
   };
 
-  // 체크박스 클릭으로 link이동이 되는 것 방지
-  const handleClick = (e: React.MouseEvent) => {
-    const target = e.target as HTMLElement;
-
-    if (target.closest('label')) {
-      e.preventDefault();
-    }
+  const onChangeTaskDone = (taskUpdateArgs: Parameters<typeof updateTask>) => {
+    taskDoneMutation.mutate(taskUpdateArgs);
   };
 
   return (
@@ -51,7 +97,6 @@ export default function KanbanCard({ taskList, tab, onDragStart }: Props) {
           isDragging && 'border-primary opacity-50',
         )}
         to={`/list${pathname}`}
-        onClick={handleClick}
       >
         <div
           className={clsx('flex items-center justify-between', {
@@ -69,11 +114,21 @@ export default function KanbanCard({ taskList, tab, onDragStart }: Props) {
           </div>
         </div>
         <div className='flex flex-col gap-2'>
-          {taskList.tasks.map((task, i) => (
+          {taskList.tasks.map(task => (
             <TaskCheckbox
               key={task.id}
-              taskId={i}
-              isDone={task.doneAt ? true : false}
+              taskId={task.id}
+              isDone={!!task.doneAt}
+              onChange={() => {
+                onChangeTaskDone([
+                  task.id,
+                  {
+                    name: task.name,
+                    description: task.description,
+                    done: !task.doneAt,
+                  },
+                ]);
+              }}
             >
               {task.name}
             </TaskCheckbox>
