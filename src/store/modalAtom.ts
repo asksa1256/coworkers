a628efbfc4ac3189ed68571c8f638e0f97918d85
tasi <taegyung.e@gmail.com>
2025-10-31 13:08:14 +0900

    fix: 모달의 children 타입 수정

--- src/store/modalAtom.ts
+++ src/store/modalAtom.ts
@@ -1,8 +1,8 @@
 import { atom } from 'jotai';
-import type { ReactNode } from 'react';
+import type { ReactElement, ReactNode } from 'react';
 
 export interface ModalOptions {
-  children: ReactNode;
+  children: ReactNode | (() => ReactElement);
   closeIconButton?: boolean; // x 닫기 버튼 유무
   className?: string; // DialogContent 병합용 클래스 네임
   mode?: 'bottomSheet' | 'normal'; // 스타일 - 모달 모드(기본값 - bottomSheet)
