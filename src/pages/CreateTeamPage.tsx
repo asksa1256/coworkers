5271fd14d9f4f66489b54c9f7f514d5a4b232431
tasi <taegyung.e@gmail.com>
2025-11-03 19:30:38 +0900

    chore: 팀생성하기 api 성공시 groups 쿼리키 캐시무효화 처리 추가

--- src/pages/CreateTeamPage.tsx
+++ src/pages/CreateTeamPage.tsx
@@ -1,18 +1,25 @@
+import { groupQueries } from '@/api/queries';
 import TeamForm, {
   type TeamFormDataType,
 } from '@/components/feature/form/TeamForm';
 import axiosInstance from '@/lib/axios';
+import { userAtom } from '@/store/authAtom';
+import { useQueryClient } from '@tanstack/react-query';
+import { useAtomValue } from 'jotai';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'sonner';
 
 export default function CreateTeamPage() {
   const nav = useNavigate();
+  const user = useAtomValue(userAtom);
+  const queryClient = useQueryClient();
 
   const handleSubmit = async (formData: TeamFormDataType) => {
     try {
       const { data } = await axiosInstance.post('/groups', formData);
 
       toast.success('팀 생성에 성공하였습니다. 생성한 팀페이지로 이동합니다.');
+      queryClient.invalidateQueries({ queryKey: groupQueries.groups(user) });
       nav(`/${data.id}`);
     } catch (error) {
       console.log(error);
