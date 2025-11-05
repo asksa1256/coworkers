bd7ec52a361aed9dda7f262c7cb48b5e10974a38
leesangdal <asksa1256@gmail.com>
2025-11-05 14:20:28 +0900

    feat: 게시글 검색 기능 추가

---
+++ src/components/feature/search/SearchField.tsx
@@ -0,0 +1,46 @@
+import SearchIcon from '@/assets/icons/SearchIcon.svg?react';
+import { cn } from '@/lib/utils';
+import { useState, type ChangeEvent, type KeyboardEvent } from 'react';
+import { useSearchParams } from 'react-router-dom';
+
+interface SearchFieldProps {
+  className?: string;
+  queryKey?: string;
+}
+
+export default function SearchField({
+  className,
+  queryKey = 'q',
+}: SearchFieldProps) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [inputValue, setInputValue] = useState('');
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSearch = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      const newSearchParams = new URLSearchParams(searchParams);
+      newSearchParams.set(queryKey, inputValue);
+      setSearchParams(newSearchParams);
+    }
+  };
+
+  return (
+    <div className={cn('relative', className)}>
+      <SearchIcon
+        aria-label='검색하기'
+        className='text-primary absolute top-1/2 left-3 h-6 w-6 -translate-y-1/2 md:left-4 md:h-8 md:w-8'
+      />
+      <input
+        type='search'
+        value={inputValue}
+        placeholder='검색어를 입력해주세요'
+        onChange={handleChange}
+        onKeyDown={handleSearch}
+        className='border-primary w-full rounded-full border-2 py-3 pl-12 text-base md:max-w-[420px] md:px-4 md:pl-[60px]'
+      />
+    </div>
+  );
+}
--- src/pages/board/BoardPage.tsx
+++ src/pages/board/BoardPage.tsx
@@ -1,10 +1,12 @@
 import ArticleList from '@/components/feature/articles/ArticleList';
+import SearchField from '@/components/feature/search/SearchField';
 
 export default function BoardPage() {
   return (
     <div className='w-full max-w-280 pt-6 pb-10 md:pt-[77px] lg:mx-auto lg:pt-[87px]'>
       <div className='mb-5 md:mb-[30px]'>
         <h2 className='text-xl font-bold md:text-2xl'>자유게시판</h2>
+        <SearchField />
       </div>
 
       <ArticleList />
