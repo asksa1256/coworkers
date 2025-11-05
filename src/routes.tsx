d8eb658d15b43fbdfd0352b5390ec956cc17b69b
tasi <taegyung.e@gmail.com>
2025-11-05 15:55:52 +0900

    chore: route 수정 및 스타일 expand-bg-x 유틸리티 클래스 수정

--- src/routes.tsx
+++ src/routes.tsx
@@ -48,7 +48,7 @@ const routes = [
             element: withPrivate(<TeamPage />),
           },
           {
-            path: 'details',
+            path: 'details/:taskListId',
             element: withPrivate(<TaskListPage />),
           },
         ],
