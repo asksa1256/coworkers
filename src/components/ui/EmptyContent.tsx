3347fd7c53022888aa8d4b3895477540ba4513ee
leesangdal <asksa1256@gmail.com>
2025-11-06 14:10:27 +0900

    refactor: 이미지 wrapper 태그 figure → div

--- src/components/ui/EmptyContent.tsx
+++ src/components/ui/EmptyContent.tsx
@@ -4,9 +4,9 @@ import type { ReactNode } from 'react';
 export default function EmptyContent({ children }: { children: ReactNode }) {
   return (
     <div className='flex flex-col items-center justify-center gap-6 md:gap-8'>
-      <figure className='w-[184px] pt-8 md:w-[324px] lg:w-auto'>
+      <div className='w-[184px] pt-8 md:w-[324px] lg:w-auto'>
         <img src={EmptyContentImg} alt='' className='h-auto w-full' />
-      </figure>
+      </div>
 
       {children}
     </div>
