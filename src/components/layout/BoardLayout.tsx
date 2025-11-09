1a60fa056c4564e45426b5699a31bb0ca817322b
asksa1256@gmail.com <asksa1256@gmail.com>
2025-11-09 22:33:43 +0900

    refactor: 자유게시판 배경색 처리 코드 수정 (view-white 적용)

--- src/components/layout/BoardLayout.tsx
+++ src/components/layout/BoardLayout.tsx
@@ -1,18 +1,9 @@
 import Header from '@/components/layout/Header';
-import { cn } from '@/lib/utils';
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 export default function BoardLayout() {
-  const location = useLocation();
-  const pathname = location.pathname;
-
   return (
-    <div
-      className={cn(
-        'mx-auto flex min-h-screen max-w-[1920px] flex-col lg:flex-row',
-        pathname === '/board' ? 'bg-bg-primary' : '',
-      )}
-    >
+    <div className='mx-auto flex min-h-screen max-w-[1920px] flex-col lg:flex-row'>
       <Header />
       <main className='flex grow-1 flex-col px-4 md:px-[26px] lg:px-[30px]'>
         <Outlet />
