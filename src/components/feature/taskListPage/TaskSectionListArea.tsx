c90f228b9126a8c654bf7c4e2b37753c73e080c9
tasi <taegyung.e@gmail.com>
2025-11-10 01:56:34 +0900

    refactor: 할일 완료 기능 mutation.ts 파일로 분리

--- src/components/feature/taskListPage/TaskSectionListArea.tsx
+++ src/components/feature/taskListPage/TaskSectionListArea.tsx
@@ -1,24 +1,15 @@
-import { updateTask } from '@/api/api';
 import { taskMutations } from '@/api/mutations';
-import { groupQueries, taskListQueries, taskQueries } from '@/api/queries';
+import { taskQueries } from '@/api/queries';
 import TaskSectionLIstItem from '@/components/feature/taskListPage/TaskSectionLIstItem';
 import EmptyContent from '@/components/ui/EmptyContent';
 import { Spinner } from '@/components/ui/spinner';
-import type {
-  TaskDetailResponse,
-  TaskListsResponse,
-  TaskUpdateRequestBody,
-} from '@/types/taskType';
+import type { TaskDetailResponse } from '@/types/taskType';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import { produce } from 'immer';
 import { useParams } from 'react-router-dom';
 
 interface Props {
   date: Date;
 }
-
-const toggleDoneAt = (task: Omit<TaskDetailResponse, 'recurring'>) =>
-  (task.doneAt = task.doneAt ? null : new Date().toLocaleString());
 
 export default function TaskSectionListArea({ date }: Props) {
   const queryClient = useQueryClient();
@@ -26,91 +17,12 @@ export default function TaskSectionListArea({ date }: Props) {
   const { data: tasksData } = useQuery(
     taskQueries.tasksOptions(groupId, taskListId, date),
   );
-  const { mutate: updateTaskDoneMutate } = useMutation({
-    mutationKey: taskMutations.updateTaskDoneMutation(Number(groupId)),
-    mutationFn: ({
-      taskId,
-      payload,
-    }: {
-      groupId: string;
-      taskListId: string;
-      taskId: number;
-      date: Date;
-      payload: TaskUpdateRequestBody;
-    }) => updateTask(taskId, payload),
-    onMutate: async ({ groupId, taskListId, taskId, date, payload }) => {
-      // 기존 쿼리들 취소
-      await queryClient.cancelQueries({
-        queryKey: taskListQueries.singleTaskList(groupId, taskListId, date),
-      });
-      await queryClient.cancelQueries({
-        queryKey: taskQueries.tasks(groupId, taskListId, date),
-      });
-
-      // 이전 상태 백업
-      const prevTaskLists = queryClient.getQueryData(
-        taskListQueries.singleTaskList(groupId, taskListId, date),
-      );
-      const prevTasks = queryClient.getQueryData(
-        taskQueries.tasks(groupId, taskListId, date),
-      );
-
-      // taskList setQueryData
-      queryClient.setQueryData(
-        taskListQueries.singleTaskList(groupId, taskListId, date),
-        (prev: TaskListsResponse) => {
-          if (!prev) return prev;
-          return produce(prev, draft => {
-            const targetTask = draft.tasks.find(task => task.id === taskId);
-            if (targetTask) toggleDoneAt(targetTask);
-          });
-        },
-      );
-
-      // tasks setQueryData
-      queryClient.setQueryData(
-        taskQueries.tasks(groupId, taskListId, date),
-        (prev: TaskDetailResponse[]) => {
-          return prev.map(prevTask =>
-            prevTask.id === taskId
-              ? { ...prevTask, doneAt: toggleDoneAt(prevTask) }
-              : prevTask,
-          );
-        },
-      );
-
-      return { prevTaskLists, prevTasks };
-    },
-    onError: (err, variables, context) => {
-      queryClient.setQueryData(
-        taskListQueries.singleTaskList(groupId, taskListId, date),
-        context?.prevTaskLists,
-      );
-      queryClient.setQueryData(
-        taskQueries.tasks(groupId, taskListId, date),
-        context?.prevTasks,
-      );
-    },
-    onSettled: () => {
-      // 뮤테이션 동시성 제어용
-      // 뮤테이션키에 해당하는 뮤테이션이 하나만 실행중일 때 캐싱 무효화 진행
-      if (
-        queryClient.isMutating({
-          mutationKey: taskMutations.updateTaskDoneMutation(Number(groupId)),
-        }) === 1
-      ) {
-        queryClient.invalidateQueries({
-          queryKey: groupQueries.group(Number(groupId)),
-        });
-        queryClient.invalidateQueries({
-          queryKey: taskListQueries.singleTaskList(groupId, taskListId, date),
-        });
-        queryClient.invalidateQueries({
-          queryKey: taskQueries.tasks(groupId, taskListId, date),
-        });
-      }
-    },
-  });
+  const { mutate: updateTaskDoneMutate } = useMutation(
+    taskMutations.updateTaskListPageTaskDoneOptions({
+      groupId: groupId ?? '',
+      queryClient,
+    }),
+  );
 
   const handleUpdateDone = (task: TaskDetailResponse) => {
     if (!groupId || !taskListId) return null;
@@ -122,7 +34,6 @@ export default function TaskSectionListArea({ date }: Props) {
     };
 
     updateTaskDoneMutate({
-      groupId,
       taskListId,
       taskId: task.id,
       date,
