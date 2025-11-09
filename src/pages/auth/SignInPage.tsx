a6345457344290c41c31ffa043f83025ba5ce6cc
asksa1256@gmail.com <asksa1256@gmail.com>
2025-11-09 22:33:13 +0900

    style: 로그인 페이지 반응형 상하 여백 추가

--- src/pages/auth/SignInPage.tsx
+++ src/pages/auth/SignInPage.tsx
@@ -2,7 +2,7 @@ import SignInForm from '@/components/feature/auth/SignInForm';
 
 export default function SignInPage() {
   return (
-    <section className='flex min-h-screen items-center justify-center'>
+    <section className='flex min-h-screen items-center justify-center py-[30px] md:py-[120px] lg:py-6'>
       <SignInForm />
     </section>
   );
